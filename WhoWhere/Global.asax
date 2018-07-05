8b0b2517a8728dcb290362bc7d5bc0ca75d83da7
Rob Wood <rdrwood@gmail.com>
2018-07-05 11:34:00 -0700

    Add project files.

---
+++ WhoWhere/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WhoWhere.MvcApplication" Language="C#" %>
---
+++ WhoWhere/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhoWhere.Startup))]
+namespace WhoWhere
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
