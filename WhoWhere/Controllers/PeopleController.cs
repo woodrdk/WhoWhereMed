102ddf3eb5ea54cb5abf70efbccf4beff5e17ef5
Rob Wood <rdrwood@gmail.com>
2018-07-12 12:51:31 -0700

    updated

---
+++ WhoWhere/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WhoWhere.Models;
+
+namespace WhoWhere.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(People peep)
+        {
+            return View();
+        }
+    }
+}
---
+++ WhoWhere/Models/People.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WhoWhere.Models
+{
+    public class People
+    {
+        // this class is to make the person object for form data
+
+        [Required(ErrorMessage = "First Name Required")]
+        [Display(Name = "First Name")]
+        [StringLength(25, ErrorMessage = "This must be 1 - 25 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Required")]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "This must be 1 - 50 characters")]
+        public string LastName { get; set; }
+
+        // value types are implicitly required because they have to have a value
+        [Display(Name = "Date of birth")]
+        public DateTime DateOfBirth { get; set; }
+
+
+        public bool Para { get; set; }
+
+    }
+}
